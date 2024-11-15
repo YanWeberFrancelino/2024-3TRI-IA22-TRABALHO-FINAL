9ef84b7d2d50c5bc4c81ef30aaca1879a817abe4
YanWeberFrancelino <106816074+YanWeberFrancelino@users.noreply.github.com>
2024-11-14 21:33:33 -0300

    Update errorHandler.ts

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -1,4 +1,3 @@
-// src/middlewares/errorHandler.ts
 import { Request, Response, NextFunction } from "express";
 
 const errorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
